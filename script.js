c4d6614a7aa87148a7ceb79759643b1b6a015a93
javibeat <javibeat@gmail.com>
2024-03-05 19:05:18 +0400

    cambios

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-// Espera a que el DOM se cargue completamente
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('menu.html') // Ruta a tu archivo menu.html
+    // Carga el menú
+    fetch('menu.html')
         .then(response => response.text())
         .then(data => {
-            // Inserta el menú en el elemento con el id 'menu-container'
             document.getElementById('menu-container').innerHTML = data;
-            // Activa cualquier script o funcionalidad adicional aquí si es necesario
+            // Después de cargar el menú, inicializa cualquier funcionalidad dependiente de jQuery
+            initializejQueryFeatures();
         })
         .catch(error => console.error(error));
 });
+
+function initializejQueryFeatures() {
+    // Asegúrate de que jQuery esté cargado
+    if (window.jQuery) {
+        // Funcionalidad de jQuery para el scroll down arrow
+        $('.scroll-down-arrow').click(function(){
+            $('html, body').animate({
+                scrollTop: $("#nextSection").offset().top
+            }, 1000); // 1000 milisegundos para el efecto de desplazamiento
+        });
+    } else {
+        console.error('jQuery is not loaded');
+    }
+}
